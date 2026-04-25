7ab4d45fb563392b7f766fd1526964356f4a89f6
Uzair-hp <mansooriujer597@gmai.com>
2026-04-25 15:56:45 +0000

    auto commit

--- run_all.sh
+++ run_all.sh
@@ -1,13 +1,22 @@
 #!/bin/bash 
 
+cd /home/user1/Desktop/devops-scripts
+
+echo "Running Tasks..."
+echo "Run Started at: $(date)"
+echo "======================"
+
 echo "Running System Monitor..."
-python3 monitor.py
+python3 monitor.py || echo "Monitor Failed"
 
 echo "Updating system..."
-bash update.sh
+bash update.sh || echo "Update Failed"
 
 echo "Pushing to Github..."
-python3 gitautomation.py
+python3 gitautomation.py || echo "Git Push Failed"
 
 echo "All Task Completed"
 
+echo "======================"
+echo "Run Completed at:$(date)"
+echo "======================="
