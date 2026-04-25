6e517950f167d747c203762920e30b2f7e9939a1
Uzair-hp <mansooriujer597@gmai.com>
2026-04-25 14:38:55 +0000

    Improved Monior Script

--- monitor.py
+++ monitor.py
@@ -1,9 +1,19 @@
 import shutil 
+import os
 
 total, used , free = shutil.disk_usage("/")
+
+print("====Disk Usage ===")
+print(f"Total: {total // (2**30)} GB")
 print(f"Used: {used // (2**30)}GB")
 print(f"Free: {free // (2**30)}GB")
 
 if free<5 *(2*30):
    print("Low Disk Space!")
+
+print("\n===Memory Usaga===")
+os.system("free -h")
+
+print("\n=== System Uptime ===")
+os.system("Uptime")
 
